2452c0f5f8368994f1395a33a13d3f03a48a1f43
Ghazwan Aliesh <ghazwan@pragamteam.dev>
2020-03-22 17:00:40 +0200

    feat: generate basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,12 @@ import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
+import { OrganizationModule } from './organization/organization.module';
 
 const dbOptions: TypeOrmModuleOptions = config.get("db");
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbOptions), AuthModule],
+  imports: [TypeOrmModule.forRoot(dbOptions), AuthModule, OrganizationModule],
   controllers: [AppController],
   providers: [AppService],
 })
